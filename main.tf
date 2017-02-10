c33b4361f01f0b3c790d7573c7375105c614697f
linux_china <linux_china@hotmail.com>
2017-02-10 14:52:38 +0800

    change cleanup_policy to delete

--- main.tf
+++ main.tf
@@ -6,12 +6,12 @@ resource "kafka_topic" "testTopic" {
   name = "testTopic"
   partitions = 1
   replication_factor = 1
-  cleanup_policy = "compact"
+  cleanup_policy = "delete"
 }
 
 resource "kafka_topic" "ticktockTopic" {
   name = "ticktockTopic"
   partitions = 1
   replication_factor = 1
-  cleanup_policy = "compact"
+  cleanup_policy = "delete"
 }
