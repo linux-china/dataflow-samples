85e3e371bebe1582004c1fa6a9f6d034286e47bc
linux_china <linux_china@hotmail.com>
2017-02-13 22:02:49 +0800

    Polishing

--- log-sink-kafka/src/test/java/com/example/LogSinkKafkaApplicationTests.java
+++ log-sink-kafka/src/test/java/com/example/LogSinkKafkaApplicationTests.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.cloud.stream.test.binder.MessageCollector;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.test.annotation.DirtiesContext;
@@ -18,14 +17,10 @@ public class LogSinkKafkaApplicationTests {
     @Autowired
     private Sink sink;
 
-    @Autowired
-    private MessageCollector messageCollector;
-
     @Test
     public void contextLoads() {
         Message<String> message = new GenericMessage<>("hello");
         sink.input().send(message);
     }
-
 
 }
