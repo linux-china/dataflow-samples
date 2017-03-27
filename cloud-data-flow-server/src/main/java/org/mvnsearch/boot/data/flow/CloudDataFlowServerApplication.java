55833f48d7862740ea3777fc2f1f4430f5f53bb8
linux_china <linux_china@hotmail.com>
2017-03-27 14:33:20 +0800

    add @EnableRedisHttpSession

--- cloud-data-flow-server/src/main/java/org/mvnsearch/boot/data/flow/CloudDataFlowServerApplication.java
+++ cloud-data-flow-server/src/main/java/org/mvnsearch/boot/data/flow/CloudDataFlowServerApplication.java
@@ -3,9 +3,11 @@ package org.mvnsearch.boot.data.flow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.dataflow.server.EnableDataFlowServer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableDataFlowServer
+@EnableRedisHttpSession
 public class CloudDataFlowServerApplication {
 
     public static void main(String[] args) {
